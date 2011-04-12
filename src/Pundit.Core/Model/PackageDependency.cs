9166e4050a3f71e7f80e73538264d7f94c6eab26
SND\aloneguid_cp <SND\aloneguid_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2011-04-12 00:14:53 +0000

    added DevTimeOnly (devtime) in PackageDependency
    git-svn-id: https://pundit.svn.codeplex.com/svn/trunk@76087 4ef1bfeb-0a34-4d02-9305-240e5b07a168

--- src/Pundit.Core/Model/PackageDependency.cs
+++ src/Pundit.Core/Model/PackageDependency.cs
@@ -26,7 +26,34 @@ namespace Pundit.Core.Model
       [XmlAttribute("version")]
       public string VersionPattern { get; set; }
 
+      /// <summary>
+      /// Dependency platform. If ommitted "noarch" assumed. If noarch is not found, no automatic
+      /// guess is performed
+      /// </summary>
       [XmlAttribute("platform")]
       public string Platform { get; set; }
+
+      /// <summary>
+      /// Set to true if the dependency must exist at dev time only.
+      /// Use it if and only if your package doesn't require the dependent package in the runtime.
+      /// This dependency won't be included in the compiled package manifest.
+      /// 
+      /// Real World Examples:
+      /// 
+      /// 1) You reference the boost.org library, however your project output is a pure so/dll.
+      /// In this case your package's consumers won't need the boost libraries as they are linked in
+      /// statically.
+      /// 
+      /// 2) You reference a "resource dll" (images, strings etc) which is embedded as a resource
+      /// in your package output, i.e. files are not read from the disk, but from a so/dll instead.
+      /// 
+      /// 3) You write a .NET library, however you decide to merge-in the dependencies inside you
+      /// library (using ILMerge or custom AppDomain loader)
+      /// 
+      /// ... (think carefully)
+      /// 
+      /// </summary>
+      [XmlAttribute("devtime")]
+      public bool DevTimeOnly { get; set; }
    }
 }
