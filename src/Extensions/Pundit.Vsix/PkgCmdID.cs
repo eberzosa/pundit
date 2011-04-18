d0828075edba658aade502fa164058cb77f0f99a
SND\aloneguid_cp <SND\aloneguid_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2011-04-18 21:12:52 +0000

    - visual studio extension template, first commit
    git-svn-id: https://pundit.svn.codeplex.com/svn/trunk@76402 4ef1bfeb-0a34-4d02-9305-240e5b07a168

---
+++ src/Extensions/Pundit.Vsix/PkgCmdID.cs
@@ -0,0 +1,11 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+namespace Pundit.Vsix
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidPunditFirstCmd =        0x100;
+        public const uint cmdidPunditFirstToolWindow =    0x101;
+
+    };
+}
