e61a8aeb4036a814734697862cc6618b57999a64
SND\aloneguid_cp <SND\aloneguid_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2011-04-25 00:41:39 +0000

    - successfull attempts to create a simple VSIX extension and extend "add reference" dialog
    git-svn-id: https://pundit.svn.codeplex.com/svn/trunk@76715 4ef1bfeb-0a34-4d02-9305-240e5b07a168

---
+++ src/Pundit.Vsix/Weirdo/Guids.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Pundit.Vsix.Weirdo
+{
+	/// <summary>
+	/// This class is used only to expose the list of Guids used by this package.
+	/// This list of guids must match the set of Guids used inside the VSCT file.
+	/// </summary>
+	internal static class GuidsList
+	{
+		// Now define the list of guids as public static members.
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        public static readonly Guid guidPunditPkg = new Guid("{3C7C5ABE-82AC-4A37-B077-0FF60E8B1FD3}");
+
+        public static readonly Guid guidPunditCmdSet = new Guid("{19492BCB-32B3-4EC3-8826-D67CD5526653}");
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+		public static readonly Guid guidGenericCmdBmp = new Guid("{0A4C51BD-3239-4370-8869-16E0AE8C0A46}");
+	}
+}
