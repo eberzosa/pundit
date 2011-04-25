4834b817a3eca2b0b3306de3d790ab511797c693
SND\aloneguid_cp <SND\aloneguid_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2011-04-25 22:28:08 +0000

    - GlobalOptionsForm draft
    git-svn-id: https://pundit.svn.codeplex.com/svn/trunk@76771 4ef1bfeb-0a34-4d02-9305-240e5b07a168

---
+++ src/Pundit.WinForms.Core/GlobalOptionsForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Pundit.WinForms.Core
+{
+   partial class GlobalOptionsForm
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.label1 = new System.Windows.Forms.Label();
+         this.textBox1 = new System.Windows.Forms.TextBox();
+         this.label2 = new System.Windows.Forms.Label();
+         this.textBox2 = new System.Windows.Forms.TextBox();
+         this.groupBox1 = new System.Windows.Forms.GroupBox();
+         this.button1 = new System.Windows.Forms.Button();
+         this.button2 = new System.Windows.Forms.Button();
+         this.listBox1 = new System.Windows.Forms.ListBox();
+         this.button3 = new System.Windows.Forms.Button();
+         this.button4 = new System.Windows.Forms.Button();
+         this.button5 = new System.Windows.Forms.Button();
+         this.button6 = new System.Windows.Forms.Button();
+         this.groupBox1.SuspendLayout();
+         this.SuspendLayout();
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(4, 7);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(108, 13);
+         this.label1.TabIndex = 0;
+         this.label1.Text = "Local repository path:";
+         // 
+         // textBox1
+         // 
+         this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+         this.textBox1.Location = new System.Drawing.Point(118, 4);
+         this.textBox1.Name = "textBox1";
+         this.textBox1.ReadOnly = true;
+         this.textBox1.Size = new System.Drawing.Size(426, 20);
+         this.textBox1.TabIndex = 1;
+         // 
+         // label2
+         // 
+         this.label2.AutoSize = true;
+         this.label2.Location = new System.Drawing.Point(45, 33);
+         this.label2.Name = "label2";
+         this.label2.Size = new System.Drawing.Size(67, 13);
+         this.label2.TabIndex = 2;
+         this.label2.Text = "Used space:";
+         // 
+         // textBox2
+         // 
+         this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+         this.textBox2.Location = new System.Drawing.Point(118, 30);
+         this.textBox2.Name = "textBox2";
+         this.textBox2.ReadOnly = true;
+         this.textBox2.Size = new System.Drawing.Size(281, 20);
+         this.textBox2.TabIndex = 3;
+         // 
+         // groupBox1
+         // 
+         this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+         this.groupBox1.Controls.Add(this.button6);
+         this.groupBox1.Controls.Add(this.button5);
+         this.groupBox1.Controls.Add(this.button4);
+         this.groupBox1.Controls.Add(this.button3);
+         this.groupBox1.Controls.Add(this.listBox1);
+         this.groupBox1.Location = new System.Drawing.Point(7, 57);
+         this.groupBox1.Name = "groupBox1";
+         this.groupBox1.Size = new System.Drawing.Size(537, 201);
+         this.groupBox1.TabIndex = 4;
+         this.groupBox1.TabStop = false;
+         this.groupBox1.Text = "Additional repositories";
+         // 
+         // button1
+         // 
+         this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+         this.button1.Location = new System.Drawing.Point(387, 264);
+         this.button1.Name = "button1";
+         this.button1.Size = new System.Drawing.Size(75, 23);
+         this.button1.TabIndex = 5;
+         this.button1.Text = "&OK";
+         this.button1.UseVisualStyleBackColor = true;
+         // 
+         // button2
+         // 
+         this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+         this.button2.Location = new System.Drawing.Point(468, 264);
+         this.button2.Name = "button2";
+         this.button2.Size = new System.Drawing.Size(75, 23);
+         this.button2.TabIndex = 6;
+         this.button2.Text = "&Cancel";
+         this.button2.UseVisualStyleBackColor = true;
+         // 
+         // listBox1
+         // 
+         this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+         this.listBox1.FormattingEnabled = true;
+         this.listBox1.Location = new System.Drawing.Point(7, 20);
+         this.listBox1.Name = "listBox1";
+         this.listBox1.Size = new System.Drawing.Size(120, 173);
+         this.listBox1.TabIndex = 0;
+         // 
+         // button3
+         // 
+         this.button3.Location = new System.Drawing.Point(134, 20);
+         this.button3.Name = "button3";
+         this.button3.Size = new System.Drawing.Size(75, 23);
+         this.button3.TabIndex = 1;
+         this.button3.Text = "button3";
+         this.button3.UseVisualStyleBackColor = true;
+         // 
+         // button4
+         // 
+         this.button4.Location = new System.Drawing.Point(134, 50);
+         this.button4.Name = "button4";
+         this.button4.Size = new System.Drawing.Size(75, 23);
+         this.button4.TabIndex = 2;
+         this.button4.Text = "button4";
+         this.button4.UseVisualStyleBackColor = true;
+         // 
+         // button5
+         // 
+         this.button5.Location = new System.Drawing.Point(134, 80);
+         this.button5.Name = "button5";
+         this.button5.Size = new System.Drawing.Size(75, 23);
+         this.button5.TabIndex = 3;
+         this.button5.Text = "button5";
+         this.button5.UseVisualStyleBackColor = true;
+         // 
+         // button6
+         // 
+         this.button6.Location = new System.Drawing.Point(134, 110);
+         this.button6.Name = "button6";
+         this.button6.Size = new System.Drawing.Size(75, 23);
+         this.button6.TabIndex = 4;
+         this.button6.Text = "button6";
+         this.button6.UseVisualStyleBackColor = true;
+         // 
+         // GlobalOptionsForm
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(548, 299);
+         this.Controls.Add(this.button2);
+         this.Controls.Add(this.button1);
+         this.Controls.Add(this.groupBox1);
+         this.Controls.Add(this.textBox2);
+         this.Controls.Add(this.label2);
+         this.Controls.Add(this.textBox1);
+         this.Controls.Add(this.label1);
+         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+         this.Name = "GlobalOptionsForm";
+         this.Text = "Pundit Global Settings";
+         this.groupBox1.ResumeLayout(false);
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.TextBox textBox1;
+      private System.Windows.Forms.Label label2;
+      private System.Windows.Forms.TextBox textBox2;
+      private System.Windows.Forms.GroupBox groupBox1;
+      private System.Windows.Forms.Button button6;
+      private System.Windows.Forms.Button button5;
+      private System.Windows.Forms.Button button4;
+      private System.Windows.Forms.Button button3;
+      private System.Windows.Forms.ListBox listBox1;
+      private System.Windows.Forms.Button button1;
+      private System.Windows.Forms.Button button2;
+   }
+}
