fac78b1e15e9f909adb0d0b86a7bf3c9d4d4f359
SND\aloneguid_cp <SND\aloneguid_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2011-10-22 21:53:33 +0000

    git-svn-id: https://pundit.svn.codeplex.com/svn/trunk@84837 4ef1bfeb-0a34-4d02-9305-240e5b07a168

---
+++ src/Pundit.Server.Core/Simple/FilesystemService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Pundit.Core.Application.Repository;
+using Pundit.Core.Model;
+
+namespace Pundit.Server.Core.Simple
+{
+   class FilesystemService : IRepository
+   {
+      private IRepository _parent;
+
+      public FilesystemService(string rootPath)
+      {
+         _parent = new FileRepository(rootPath);
+      }
+
+      public void Publish(Stream packageStream)
+      {
+         throw new NotImplementedException();
+      }
+
+      public Stream Download(PackageKey key)
+      {
+         throw new NotImplementedException();
+      }
+
+      public Version[] GetVersions(UnresolvedPackage package, VersionPattern pattern)
+      {
+         throw new NotImplementedException();
+      }
+
+      public Package GetManifest(PackageKey key)
+      {
+         throw new NotImplementedException();
+      }
+
+      public bool[] PackagesExist(PackageKey[] packages)
+      {
+         throw new NotImplementedException();
+      }
+
+      public PackageKey[] Search(string substring)
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
---
+++ src/Pundit.Server.Core/StandaloneHost.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pundit.Server.Core
+{
+   public class StandaloneHost
+   {
+   }
+}
