d3b827dabdd4841e38e9ce38f4503a93b05fdd2a
SND\aloneguid_cp <SND\aloneguid_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2011-11-02 16:34:23 +0000

    git-svn-id: https://pundit.svn.codeplex.com/svn/trunk@85246 4ef1bfeb-0a34-4d02-9305-240e5b07a168

--- src/Pundit.Core/Application/Repository/SqliteExtensions.cs
+++ src/Pundit.Core/Application/Repository/SqliteExtensions.cs
@@ -27,5 +27,44 @@ namespace System.Data.SQLite
          cmd.Parameters.Add(p);
          return cmd;
       }
+	  
+	        public static string AsString(this DbDataReader reader, string columnName)
+      {
+         object value = reader[columnName];
+         if (value == null || value is DBNull) return null;
+         return (string) value;
+      }
+
+      public static bool AsBool(this DbDataReader reader, string columnName)
+      {
+         object value = reader[columnName];
+         if (value == null || value is DBNull) return false;
+         if (value is long) return (long)value != 0;
+         if (value is bool) return (bool)value;
+         throw new ArgumentException("can't cast from " + value.GetType() + " to bool");
+      }
+
+      public static long AsLong(this DbDataReader reader, string columnName)
+      {
+         object value = reader[columnName];
+         if (value == null || value is DBNull) return 0;
+         if (value is long) return (long) value;
+         return 0;
+      }
+
+      public static DateTime? AsNullableDateTime(this DbDataReader reader, string columnName)
+      {
+         object value = reader[columnName];
+         if (value == null || value is DBNull) return null;
+         if (value is DateTime) return (DateTime) value;
+         throw new ArgumentException("unexpected type " + value.GetType() + " (expected " + typeof(DateTime) + ")");
+      }
+
+      public static DateTime AsDateTime(this DbDataReader reader, string columnName)
+      {
+         DateTime? v = AsNullableDateTime(reader, columnName);
+         if (v == null) return DateTime.MinValue;
+         return v.Value;
+      }
    }
 }
