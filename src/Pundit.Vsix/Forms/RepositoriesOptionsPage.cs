0e3dcd91b12d3e0094b866ecd3018dda95319c68
SND\aloneguid_cp <SND\aloneguid_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2011-11-13 04:30:56 +0000

    git-svn-id: https://pundit.svn.codeplex.com/svn/trunk@85510 4ef1bfeb-0a34-4d02-9305-240e5b07a168

--- src/Pundit.Vsix/Forms/RepositoriesOptionsPage.cs
+++ src/Pundit.Vsix/Forms/RepositoriesOptionsPage.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -50,4 +50,4 @@ namespace Pundit.Vsix.Forms
          base.OnApply(e);
       }
    }
-}
+}*/
