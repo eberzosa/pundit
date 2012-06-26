12fa5f0b249a6172792d826dcb64ee7a3e178460
SND\aloneguid_cp <SND\aloneguid_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2012-06-26 18:42:47 +0000

    splitting unit test projects
    git-svn-id: https://pundit.svn.codeplex.com/svn/trunk@93030 4ef1bfeb-0a34-4d02-9305-240e5b07a168

---
+++ src/test/Pundit.Test.Server/Import/NuGetImporterTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using NUnit.Framework;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace Pundit.Test.Import
+{
+   //those are only a bunch or raw experiments for now
+
+   [TestFixture, Ignore]
+   class NuGetImporterTest
+   {
+      [Test]
+      public void ImportListTest()
+      {
+         var client = new RestClient("https://nuget.org/api/v2");
+         //first page only
+         var request =
+            new RestRequest(
+               "Packages()?$orderby=Id&$filter=IsLatestVersion",  
+               Method.GET);
+         IRestResponse response = client.Execute(request);
+         JObject all = JObject.Parse(response.Content);
+      }
+   }
+}
