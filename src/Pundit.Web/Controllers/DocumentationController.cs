721bf6a9fb0d418fa7e50f0c53da18b08f769beb
SND\aloneguid_cp <SND\aloneguid_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2012-07-09 21:14:10 +0000

    documentation
    git-svn-id: https://pundit.svn.codeplex.com/svn/trunk@93605 4ef1bfeb-0a34-4d02-9305-240e5b07a168

--- src/Pundit.Web/Controllers/DocumentationController.cs
+++ src/Pundit.Web/Controllers/DocumentationController.cs
@@ -1,20 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Pundit.Web.Controllers
 {
-    public class DocumentationController : Controller
-    {
-        //
-        // GET: /Documentation/
+   public class DocumentationController : Controller
+   {
+      public ActionResult Index()
+      {
+         return View();
+      }
 
-        public ActionResult Index()
-        {
-            return View();
-        }
+      public ActionResult Installation()
+      {
+         return View();
+      }
 
-    }
+      public ActionResult GettingStartedIn5()
+      {
+         return View();
+      }
+
+      public ActionResult GettingStartedIn30()
+      {
+         return View();
+      }
+   }
 }
