e20e92823bf3d9021ad387bb4d7fc7a167ae9514
Enrique Berzosa <eberzosa@mimecast.com>
2018-11-05 16:04:59 +0000

    Added E2E test files

--- test/pack.cmd
+++ test/pack.cmd
@@ -5,5 +5,4 @@
 %pundit% pack pundit-MyApplication.Test.xml
 %pundit% pack pundit-MyApplication.Tool.xml
 
-copy MyApplication.*.pundit C:\Users\Enrique\.pundit\remote
-del MyApplication.*.pundit
+move MyApplication.*.nupkg repo
