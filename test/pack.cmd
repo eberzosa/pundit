dc15cbd75f1e068054a98644d42c42fd873985a5
Enrique Berzosa <eberzosa@mimecast.com>
2018-12-14 22:09:58 +0000

    Changes in the tests to revert not used "lib" change

--- test/pack.cmd
+++ test/pack.cmd
@@ -1,9 +1,10 @@
 
-@set pundit=..\src\Pundit\bin\Debug\pundit 
+@set pundit=..\src\Pundit\bin\Debug\net461\pundit 
 
 %pundit% pack pundit-MyApplication.Common.xml
 %pundit% pack pundit-MyApplication.Test.xml
 %pundit% pack pundit-MyApplication.Tool.xml
+%pundit% pack pundit-MyApplication.ConfigTest.xml
 %pundit% pack pundit-MultiFramework.xml
 
 move MyApplication.*.nupkg repo
